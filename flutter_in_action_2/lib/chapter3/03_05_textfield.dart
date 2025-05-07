7bb04b591840d3db16959ab3bc5caf675756ce46
sumilao <494013647@qq.com>
2025-05-07 20:19:24 +0800

    通过

--- flutter_in_action_2/lib/chapter3/03_05_textfield.dart
+++ flutter_in_action_2/lib/chapter3/03_05_textfield.dart
@@ -33,63 +33,63 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        children: <Widget>[
-          TextField(
-            autofocus: true,
-            focusNode: focusNode1, //关联focusNode1
-            toolbarOptions: const ToolbarOptions(
-              selectAll: true,
-              copy: true,
-              paste: true,
-            ),
-            decoration: const InputDecoration(labelText: "input1"),
-          ),
-          TextField(
-            focusNode: focusNode2, //关联focusNode2
-            decoration: const InputDecoration(labelText: "input2"),
-          ),
-          Builder(
-            builder: (ctx) {
-              return Column(
-                children: <Widget>[
-                  ElevatedButton(
-                    child: const Text("移动焦点"),
-                    onPressed: () {
-                      //将焦点从第一个TextField移到第二个TextField
-                      FocusScope.of(context).requestFocus(focusNode2);
-                    },
-                  ),
-                  ElevatedButton(
-                    child: const Text("隐藏键盘"),
-                    onPressed: () {
-                      // 当所有编辑框都失去焦点时键盘就会收起
-                      focusNode1.unfocus();
-                      focusNode2.unfocus();
-                    },
-                  ),
-                ],
-              );
-            },
-          ),
-          const Text("自定义下划线颜色"),
-          const TextField(
-            decoration: InputDecoration(
-              labelText: "请输入用户名",
-              prefixIcon: Icon(Icons.person),
-              enabledBorder: UnderlineInputBorder(
-                borderSide: BorderSide(color: Colors.green),
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            TextField(
+              autofocus: true,
+              focusNode: focusNode1, //关联focusNode1
+              toolbarOptions: const ToolbarOptions(
+                selectAll: true,
+                copy: true,
+                paste: true,
               ),
-              focusedBorder: UnderlineInputBorder(
-                borderSide: BorderSide(color: Colors.red),
+              decoration: const InputDecoration(labelText: "input1"),
+            ),
+            TextField(
+              focusNode: focusNode2, //关联focusNode2
+              decoration: const InputDecoration(labelText: "input2"),
+            ),
+            Builder(
+              builder: (ctx) {
+                return Column(
+                  children: <Widget>[
+                    ElevatedButton(
+                      child: const Text("移动焦点"),
+                      onPressed: () {
+                        //将焦点从第一个TextField移到第二个TextField
+                        FocusScope.of(context).requestFocus(focusNode2);
+                      },
+                    ),
+                    ElevatedButton(
+                      child: const Text("隐藏键盘"),
+                      onPressed: () {
+                        // 当所有编辑框都失去焦点时键盘就会收起
+                        focusNode1.unfocus();
+                        focusNode2.unfocus();
+                      },
+                    ),
+                  ],
+                );
+              },
+            ),
+            const Text("自定义下划线颜色"),
+            const TextField(
+              decoration: InputDecoration(
+                labelText: "请输入用户名",
+                prefixIcon: Icon(Icons.person),
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.green),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.red),
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
 }
-
