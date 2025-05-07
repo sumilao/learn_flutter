2d0875a0d0000801428d81646fc3a52aad9a70b8
sumilao <494013647@qq.com>
2025-05-07 08:50:19 +0800

    update testfield

---
+++ flutter_in_action_2/lib/chapter3/03_05_textfield.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  FocusNode focusNode1 = FocusNode();
+  FocusNode focusNode2 = FocusNode();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        children: <Widget>[
+          TextField(
+            autofocus: true,
+            focusNode: focusNode1, //关联focusNode1
+            toolbarOptions: const ToolbarOptions(
+              selectAll: true,
+              copy: true,
+              paste: true,
+            ),
+            decoration: const InputDecoration(labelText: "input1"),
+          ),
+          TextField(
+            focusNode: focusNode2, //关联focusNode2
+            decoration: const InputDecoration(labelText: "input2"),
+          ),
+          Builder(
+            builder: (ctx) {
+              return Column(
+                children: <Widget>[
+                  ElevatedButton(
+                    child: const Text("移动焦点"),
+                    onPressed: () {
+                      //将焦点从第一个TextField移到第二个TextField
+                      FocusScope.of(context).requestFocus(focusNode2);
+                    },
+                  ),
+                  ElevatedButton(
+                    child: const Text("隐藏键盘"),
+                    onPressed: () {
+                      // 当所有编辑框都失去焦点时键盘就会收起
+                      focusNode1.unfocus();
+                      focusNode2.unfocus();
+                    },
+                  ),
+                ],
+              );
+            },
+          ),
+          const Text("自定义下划线颜色"),
+          const TextField(
+            decoration: InputDecoration(
+              labelText: "请输入用户名",
+              prefixIcon: Icon(Icons.person),
+              enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.green),
+              ),
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.red),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
