f8994d6420e4cfeba802c323d78b7c38c4ee294a
sumilao <494013647@qq.com>
2025-05-08 23:11:19 +0800

    添加form

--- flutter_in_action_2/lib/chapter3/03_05_textfield.dart
+++ flutter_in_action_2/lib/chapter3/03_05_textfield.dart
@@ -34,6 +34,10 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
       body: Center(
         child: Column(
           children: <Widget>[
