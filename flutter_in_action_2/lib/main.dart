06df97167d66d0455a10dcfd3243ce8337ec434b
sumilao <494013647@qq.com>
2025-05-11 08:57:39 +0800

    添加route和page

--- flutter_in_action_2/lib/main.dart
+++ flutter_in_action_2/lib/main.dart
@@ -30,13 +30,45 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+// 定义 Page 类
+class Page {
+  final String title;
+  final Widget widget;
+  final bool withScaffold;
+
+  const Page(this.title, this.widget, {this.withScaffold = true});
+
+  void openPage(BuildContext context) {
+    if (withScaffold) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => Scaffold(
+            appBar: AppBar(
+              title: Text(title),
+            ),
+            body: widget,
+          ),
+        ),
+      );
+    } else {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => widget,
+        ),
+      );
+    }
+  }
+}
+
 class _MyHomePageState extends State<MyHomePage> {
-  List<Widget> _generateItem(BuildContext context, List<Page<Object?>> children) {
+  List<Widget> _generateItem(BuildContext context, List<Page> children) {
     return children.map<Widget>((page) {
       return ListTile(
-        // title: Text(page.title),
+        title: Text(page.title),
         trailing: const Icon(Icons.keyboard_arrow_right),
-        // onTap: () => page.openPage(context),
+        onTap: () => page.openPage(context),
       );
     }).toList();
   }
@@ -54,22 +86,12 @@ class _MyHomePageState extends State<MyHomePage> {
             title: const Text("第一个Flutter应用"),
             children: _generateItem(context, [
               // Page("计数器", const CounterRoute(), withScaffold: false),
-        MaterialPage<void>(
-          key: const ValueKey<String>('路由传值'),
-          child: const RouterTestRoute(),
-        ),
-              // MaterialPage<void>("路由传值", const RouterTestRoute()),
+              Page("路由传值", const RouterTestRoute()),
               // Page("State生命周期", const StateLifecycleTest()),
-              // Page(
-              //   "子树中获取State对象",
-              //   const GetStateObjectRoute(),
-              //   withScaffold: false,
-              // ),
-              // Page(
-              //   "Cupertino Demo",
-              //   const CupertinoTestRoute(),
-              //   withScaffold: false,
-              // ),
+              // Page("子树中获取State对象", const GetStateObjectRoute(),
+              // withScaffold: false),
+              // Page("Cupertino Demo", const CupertinoTestRoute(),
+              // withScaffold: false),
             ]),
           ),
         ],
