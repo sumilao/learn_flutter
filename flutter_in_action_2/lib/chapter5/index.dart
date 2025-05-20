0f4f8047dae8caa9ea4dbc3849a0c94f449c3bf5
sumilao <494013647@qq.com>
2025-05-20 23:52:09 +0800

    上传第6章

--- flutter_in_action_2/lib/chapter5/index.dart
+++ flutter_in_action_2/lib/chapter5/index.dart
@@ -1,7 +1,7 @@
-export 'decoratedbox.dart';
-export 'padding.dart';
-export 'scaffold.dart';
-export 'transform.dart';
-export 'container.dart';
-export 'clip.dart';
-export 'fittedbox.dart';
+export '05_01_padding.dart';
+export '05_02_decoratedbox.dart';
+export '05_03_transform.dart';
+export '05_04_clip.dart';
+export '05_05_container.dart';
+export '05_06_fittedbox.dart';
+export '05_07_scaffold.dart';
--- flutter_in_action_2/lib/routes.dart
+++ flutter_in_action_2/lib/routes.dart
@@ -1,3 +1,5 @@
 export 'chapter2/index.dart';
 export 'chapter3/index.dart';
 export 'chapter4/index.dart';
+export 'chapter5/index.dart';
+export 'chapter6/index.dart';
